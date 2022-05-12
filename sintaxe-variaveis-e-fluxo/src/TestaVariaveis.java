d18fb6e20e49b8397a4a5b96e29c8fc4eb4f037c
nora <ozielhumasi@gmail.com>
2022-05-12 09:34:38 -0300

    Segundo programa.

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,21 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá! Mais um teste.");
+		
+		int idade = 45;
+		
+		System.out.println(idade);
+		
+		idade += 10;
+		
+		System.out.println(idade);
+		
+		idade *= 2;
+		
+		System.out.println(idade);
+	}
+
+}
